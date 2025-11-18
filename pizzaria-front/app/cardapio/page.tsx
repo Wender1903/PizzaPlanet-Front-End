9b0ac5f589483ed42a6400a5722978e045019977
Wender1903 <wenderman74@gmail.com>
2025-11-18 20:59:07 -0300

    :sparkles: feat: Cria tela de cardapio

---
+++ pizzaria-front/app/cardapio/page.tsx
@@ -0,0 +1,17 @@
+export default function Cardapio() {
+  return (
+    <div className="min-h-screen bg-gray-950 flex items-center justify-center p-10">
+      <div className="bg-gray-900 shadow-2xl shadow-green-500/40 rounded-lg p-12 max-w-3xl text-center">
+        <h1 className="text-4xl font-extrabold text-green-400 tracking-wide mb-4">
+          Página de Cardápio
+        </h1>
+
+        <p className="text-gray-300 text-lg leading-relaxed">
+          Aqui no futuro você poderá visualizar, editar e gerenciar todos os itens do cardápio intergaláctico da Pizza Planet.
+          <br />
+          🍕✨
+        </p>
+      </div>
+    </div>
+  );
+}
