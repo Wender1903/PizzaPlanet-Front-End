fa7cbd577e04d9e7b10b7b38b3cde93fef6a4995
Wender1903 <wenderman74@gmail.com>
2025-11-18 21:01:37 -0300

    :package: chore: Adiciona Dicionário de pizzas para testes

---
+++ pizzaria-front/data/cardapio.js
@@ -0,0 +1,23 @@
+export const cardapio = [
+  {
+    id: 1,
+    nome: "Pizza Margherita",
+    descricao: "Molho de tomate, mussarela e manjericão fresco.",
+    preco: 29.90,
+    img: "https://media.istockphoto.com/id/686957124/pt/foto/pizza-margherita.jpg?s=612x612&w=0&k=20&c=z30tlfLNJ4Y1gWKE2SfT98qFD6A4pe-WGF_SdNuNRzY=", 
+  },
+  {
+    id: 2,
+    nome: "Pepperoni Espacial",
+    descricao: "Queijo derretido com fatias generosas de pepperoni.",
+    preco: 34.90,
+    img: "https://images.unsplash.com/photo-1628840042765-356cda07504e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVwcGVyb25pJTIwcGl6emF8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    id: 3,
+    nome: "Frango com Catupiry Galáctico",
+    descricao: "Frango desfiado, catupiry e milho.",
+    preco: 36.50,
+    img: "https://blog.fornettostore.com.br/wp-content/uploads/2025/03/blog-receita-frango-catupiry.jpg",
+  },
+];
